79b80b84e9232a2bb24eedfc441e1f6d5b325f9b
Peter Sudmant <psudmant@gmail.com>
2013-10-31 22:43:04 -0700

    clustered, then fit w/ Gaussians

--- genotyper.py
+++ genotyper.py
@@ -22,6 +22,9 @@ import math
 import random
 from scipy.stats import norm
 
+import scipy.spatial.distance as dist
+import scipy.cluster.hierarchy as hclust
+
 from wnd_cp_data import wnd_cp_indiv
 from gglob import gglob
 
@@ -59,12 +62,12 @@ class genotype_table:
 class genotyper:
     
     def init_on_indiv_DTS_files(self, **kwargs):
-        self.dts_dir = kwargs.get(dts_dir, None) 
-        self.sunk_dts_dir = kwargs.get(sunk_dts_dir, None) 
-        self.fn_contigs  = kwargs.get(fn_contigs, None) 
-        self.fn_sunk_contigs  = kwargs.get(fn_sunk_contigs, None) 
-        self.wnd_size  = kwargs.get(wnd_size, None) 
-        self.F_fnToIndiv = kwargs.get(F_fnToIndiv, lambda x: x.split("/")[-1].replace("500_bp_","")
+        self.dts_dir = kwargs.get("dts_dir", None) 
+        self.sunk_dts_dir = kwargs.get("sunk_dts_dir", None) 
+        self.fn_contigs  = kwargs.get("fn_contigs", None) 
+        self.fn_sunk_contigs  = kwargs.get("fn_sunk_contigs", None) 
+        self.wnd_size  = kwargs.get("wnd_size", None) 
+        self.F_fnToIndiv = kwargs.get("F_fnToIndiv", lambda x: x.split("/")[-1].replace("500_bp_",""))
         
         self.i_by_indiv = {}
         fn_DTSs = glob.glob("%s/%s*"%(dts_dir, DTS_prefix))
@@ -122,8 +125,8 @@ class genotyper:
 
     def __init__(self, contig, **kwargs): 
 
-        self.gglob_dir = kwargs.get(gglob_dir, None) 
-        self.plot_dir  = kwargs.get(plot_dir, None)
+        self.gglob_dir = kwargs.get("gglob_dir", None) 
+        self.plot_dir  = kwargs.get("plot_dir", None)
         
         self.contig = contig
         self.indivs = []
@@ -136,7 +139,7 @@ class genotyper:
         self.sunk_cp_matrix = None
         
         if self.gglob_dir:
-            self.init_on_gglob(self.contig, self.gglob_dir) 
+            self.init_on_gglob(self.gglob_dir, self.contig) 
         else:
             self.init_on_indiv_DTS_files(self, **kwargs)
 
@@ -160,26 +163,25 @@ class genotyper:
             ax.plot(G_x,G_y,color=c)
             ax.plot(mu,-.001,"^",ms=10,alpha=.7,color=c)
             
-    def plot(self, X, Xs, gmmX, gmmXs, v_bndsX, v_bndsXs, chr, start, end):
+    def aug_dendrogram(self, ax, ddata):
+        for i, d in zip(ddata['icoord'], ddata['dcoord']):
+            x = 0.5 * sum(i[1:3])
+            y = d[1]
+            ax.plot(x, y, 'ro')
+            ax.annotate("%.3g" % y, (x, y), xytext=(0, -8),
+                         textcoords='offset points',
+                         va='top', ha='center')
+
+    def plot(self, X, Xs, gmmX, gmmXs, v_bndsX, v_bndsXs, Z, Zs, chr, start, end):
         
         cps = np.mean(X, 1)
         sunk_cps = np.mean(Xs, 1)
         
         plt.rc('grid',color='0.75',linestyle='l',linewidth='0.1')
-        fig, axarr = plt.subplots(2, 2)
+        fig, axarr = plt.subplots(2, 3)
         fig.set_figwidth(11)
         fig.set_figheight(8.5)
         axescolor  = '#f6f6f6'
-        #h_margin = 0.05
-        #v_margin = 0.05
-        #plot_height=1-2*v_margin
-        #plot_width=((1-2*h_margin)-2*h_margin)/3.0
-        #bottom=1-h_margin-plot_height
-        #plot_rects = []
-        #plot_rect=[h_margin,bottom,plot_width,plot_height]
-        #ax = fig.add_axes(plot_rect)
-        #
-        #ax.plot(cps, sunk_cps, 'ro', alpha=0.5)
         
         axarr[0,0].plot(cps, sunk_cps, 'ro', alpha=0.2)
         axarr[0,0].set_xlim(-0.10,max(cps)+1)
@@ -187,10 +189,18 @@ class genotyper:
         
         n, bins, patches = axarr[0,1].hist(cps,alpha=.9,ec='none',normed=1,color='r',bins=len(cps)/20)
         self.addGMM(gmmX, axarr[0,1], cps)
+        fig.sca(axarr[0,2]) 
+        dendro = hclust.dendrogram(Z, orientation='right')
+        #self.aug_dendrogram(axarr[0,2], dendro)
+
         n, bins, patches = axarr[1,0].hist(sunk_cps,alpha=.9,ec='none',normed=1,color='g',bins=len(cps)/20)
         self.addGMM(gmmXs, axarr[1,0], sunk_cps)
         axarr[1,1].plot(v_bndsX[0], v_bndsX[1], 'ro-')
         axarr[1,1].plot(v_bndsXs[0], v_bndsXs[1], 'go-')
+        
+        fig.sca(axarr[1,2]) 
+        dendro = hclust.dendrogram(Zs, orientation='right')
+        #self.aug_dendrogram(axarr[1,2], dendro)
         
         fig.savefig("%s/%s-%d-%d.png"%(self.plot_dir, chr, start, end))
         plt.close()
@@ -248,8 +258,7 @@ class genotyper:
     
     def get_sunk_gt_matrix(self, contig, start, end, vb=False):
         assert contig == self.contig
-        assert self.has_sunk_cps == True
-
+        
         start_idx = np.searchsorted(self.sunk_wnd_starts, start)
         end_idx = np.searchsorted(self.sunk_wnd_ends, end)
         
@@ -270,13 +279,16 @@ class genotyper:
     def s_score(self, X, labels):
         return metrics.silhouette_score(X, labels) 
     
-
-    def fit_GMM(self, X, init_means, init_weights):
+    def fit_GMM(self, X, init_means, init_vars, init_weights):
     
         n_components = len(init_means)
         gmm = mixture.GMM(n_components, 'spherical')
         gmm.means = np.reshape(np.array(init_means),(len(init_means),1))
         gmm.weights = np.array(init_weights)
+        
+        #vars = np.array([v[0][0] for v in gmm.covars])
+        #gmm.covars = np.reshape()
+
         gmm.fit(X, n_iter=1000, init_params='c')
         labels = gmm.predict(X)
         
@@ -285,7 +297,7 @@ class genotyper:
         
         return gmm, labels, bic 
 
-    def GMM_genotype(self, X):
+    def GMM_genotype(self, X, FOUT = None):
         """
         GMM genotyping
         #cv_types = ['spherical', 'tied', 'diag', 'full']
@@ -294,25 +306,72 @@ class genotyper:
         #mus = np.median(X,1)
         mus = np.reshape(mus, (mus.shape[0],1))
         
-        round_mus = np.around(mus) 
-        init_mus = list(np.unique(round_mus))
+        #round_mus = np.around(mus) 
+        #init_mus = list(np.unique(round_mus))
+        #
+        #init_weights = []
+        #for comp in init_mus:
+        #    init_weights.append(float(np.sum(round_mus==comp))/len(round_mus))
         
-        init_weights = []
-        for comp in init_mus:
-            init_weights.append(float(np.sum(round_mus==comp))/len(round_mus))
+        dist_mat = dist.pdist(mus)
+        #Z = hclust.linkage(dist_mat, method='single')
+        #Z = hclust.linkage(dist_mat, method='single')
+        #Z = hclust.linkage(dist_mat, method='average')
+        Z = hclust.linkage(mus, method='centroid', metric='euclidean')
         
-        gmm, labels, ic = self.fit_GMM(mus, init_mus, init_weights)
+        params, bics, gmms, all_labels = [], [], [], []
         
-        print self.pw_GMM_overlap(gmm)
-        return gmm, labels, [[len(init_mus)], [ic]]
+        for k in np.arange(.2, 0.7,  0.01):
+            grps = hclust.fcluster(Z, k, criterion='distance')
+            init_mus, init_vars, init_weights = self.initialize(mus, grps) 
+            gmm, labels, ic = self.fit_GMM(mus, init_mus, init_vars, init_weights)
+            params.append(len(init_mus))
+            bics.append(ic)
+            gmms.append(gmm)
+            all_labels.append(labels)
+        
+        grps = np.zeros(mus.shape[0])
+        init_mus, init_vars, init_weights = self.initialize(mus, grps) 
+        gmm, labels, ic = self.fit_GMM(mus, init_mus, init_vars, init_weights)
+        params.append(len(init_mus))
+        bics.append(ic)
+        gmms.append(gmm)
+        all_labels.append(labels)
+        
+        #overlaps = self.pw_GMM_overlap(gmm)
+        
+        #if FOUT: 
+        #    FOUT.write(" ".join(["%f"%(max(o)) for o in overlaps]))
+        idx = np.argmin(bics)
+        gmm = gmms[idx]
+        labels = all_labels[idx]
+        return gmm, labels, Z, [params, bics]
+        #return gmm, labels, Z, [[len(init_mus)], [ic]]
     
+    def initialize(self, X, grps):
+
+        uniqs = np.unique(grps)
+        init_mus =  []
+        init_weights =  []
+        init_vars =  []
+        
+        l = X.shape[0] 
+        for grp in uniqs:
+            init_mus.append(np.mean(X[grps==grp]))
+            init_vars.append(np.var(X[grps==grp]))
+            init_weights.append(float(np.sum(grps==grp))/l)
+        
+        return init_mus, init_vars, init_weights
+        
     def pw_GMM_overlap(self, gmm):
         
         overlaps = []       
         mus = gmm.means[:,0]
         vars = np.array([v[0][0] for v in gmm.covars])
+        weights = np.array(gmm.weights)
         order = np.argsort(mus)
         mus = mus[order]
+        weights = weights[order]
         vars = vars[order]
         
         for i in xrange(mus.shape[0]-1):
@@ -324,11 +383,38 @@ class genotyper:
             mn = min(mu1, mu2) - 10*sd_max
             mx = max(mu1, mu2) + 10*sd_max
             xs = np.arange(mn,mx,0.01)
-            o = np.sum(np.min(np.c_[norm.pdf(xs,loc=mu1,scale=v1), 
-                                    norm.pdf(xs,loc=mu2,scale=v2)], 1))  * 0.01
-            print mn, mx, mu1, mu2, v1, v2, o
-            overlaps.append(o)
+            o = np.sum(np.min(np.c_[norm.pdf(xs,loc=mu1,scale=v1)*weights[i], 
+                                    norm.pdf(xs,loc=mu2,scale=v2)*weights[i+1]], 1))  * 0.01
+            overlaps.append([o,o/weights[i],o/weights[i+1]])
         return overlaps
+    
+    def __plot(self, Z, cutoff):
+        
+        dendro = hclust.dendrogram(Z, orientation='right', labels = lbls, color_threshold = cutoff)
+        grps = hclust.fcluster(Z, cutoff, criterion='distance')
+        
+        plt.gcf().set_size_inches(14,6)
+        ax=plt.gcf().gca()
+        ax.set_position([.05,0.05,.3,.9])
+
+        ax2 = plt.gcf().add_axes([.55,.05,.4,.9])
+        k = 0
+        colors = ['b','g','r','c','m','y','k']
+        n_colors = len(colors)
+
+        for clust in Set(list(grps)):
+            for idx in np.where(grps == clust)[0]:
+                ax2.plot([self.calls[idx]['start'], self.calls[idx]['end']],
+                         [k,k], 
+                         lw=1,
+                         color=colors[(clust-1)%n_colors])
+                k+=1.2
+          
+        ax2.set_xlim([min(self.all_starts),max(self.all_ends)])
+        ax2.set_ylim([-1,k+1])
+
+        plt.savefig('test.png')
+        plt.gcf().clear()
 
     def ms_genotype(self, X):
         """
